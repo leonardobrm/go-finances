b3766dc8dcaa44f87af2e05e7cba89c05a4923b6
leonardo <leonardo.brasil59@outlook.com>
2020-11-30 15:37:23 -0300

    :tada: first commit

---
+++ web/src/utils/formatValue.ts
@@ -0,0 +1,9 @@
+const formatValue = (value: number): string => {
+  const convert = Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value);
+  return convert;
+};
+
+export default formatValue;
